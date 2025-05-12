f0816e6bc59ac87cbd279fa8c6ed83619614ee2c
zakaryajawish <zakaryajawish@gmail>
2025-05-12 17:28:15 +0300

    create dashboard file - add adabtive layout widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard_x/views/dashboard_view.dart';
 
 void main() {
   runApp(const DashboardUI());
@@ -6,13 +7,12 @@ void main() {
 
 class DashboardUI extends StatelessWidget {
   const DashboardUI({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: const Text('Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Admin Dashboard',
+      home: DashboardView(),
     );
   }
 }
