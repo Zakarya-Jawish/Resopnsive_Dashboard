7bcb88cb578568fd742ce2df6d90af15efc87c19
zakaryajawish <zakaryajawish@gmail>
2025-05-13 14:27:31 +0300

    create user info list tile

---
+++ lib/views/widgets/custom_drawer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard_x/utils/app_images.dart';
+import 'package:responsive_dashboard_x/views/widgets/user_info_list_tile.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: const Column(
+        children: [
+          UserInfoListTile(
+            image: Assets.avatar1,
+            title: 'Lekan Okeowo',
+            subtitle: 'demo@gmail.com',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/widgets/user_info_list_tile.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dashboard_x/utils/app_styles.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+  final String image, title, subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(image),
+      title: Text(title, style: AppStyles.styleSemiBold16(context)),
+      subtitle: Text(
+        subtitle,
+        style: AppStyles.styleRegular12(context),
+      ),
+    );
+  }
+}
