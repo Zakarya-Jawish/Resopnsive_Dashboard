11d51fcecdcd4facf4c943c4ddcbda3876110a90
zakaryajawish <zakaryajawish@gmail>
2025-05-13 14:05:45 +0300

    generate assets class

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,24 @@
+class Assets {
+  Assets._();
+
+  static const String avatar1 = "assets/images/avatar_1.svg";
+  static const String avatar2 = "assets/images/avatar_2.svg";
+  static const String avatar3 = "assets/images/avatar_3.svg";
+  static const String balance = "assets/images/balance.svg";
+  static const String card = "assets/images/card.svg";
+  static const String cardBackground =
+      "assets/images/card_background.png";
+  static const String dashboard = "assets/images/dashboard.svg";
+  static const String expenses = "assets/images/expenses.svg";
+  static const String gallery = "assets/images/gallery.svg";
+  static const String income = "assets/images/income.svg";
+  static const String logout = "assets/images/logout.svg";
+  static const String myInvestments =
+      "assets/images/my_investments.svg";
+  static const String myTransctions =
+      "assets/images/my_transctions.svg";
+  static const String settings = "assets/images/settings.svg";
+  static const String statistics = "assets/images/statistics.svg";
+  static const String walletAccount =
+      "assets/images/wallet_account.svg";
+}
